f1b97a511bff0e1ecefd727dec50e47523e57d90
delashu <53063128+delashu@users.noreply.github.com>
2021-11-01 12:09:02 -0400

    MICE file addition

--- scripts/R/processing/mice.R
+++ scripts/R/processing/mice.R
@@ -1,49 +1,47 @@
 library(dplyr)
 library(mice)
 #read in data to perform 'mice' on  
-
 dat <- readRDS("processed_001HRS.rds")
 
 
 '
 We assume that data is Missing at Random (MAR)
-
 Thus, we will utilize mice to impute all missing independent variables
+https://cran.r-project.org/web/packages/mice/mice.pdf
 '
+
 #check for missingness before imputation
 sapply(dat, function(x) sum(is.na(x)))
 
 
-#set methods of imputation for specific variables: 
-
-
-# meth[c("life_satisfaction")]="norm" 
-# meth[c("health_rate")]="logreg" 
-# meth[c("high_BP")]="polyreg"
-# meth[c("diabetes")]="polyreg"
-# meth[c("cancer")]="polyreg"
-# meth[c("depression")]="polyreg"
-# meth[c("days_in_bed")]="polyreg"
-# meth[c("memory")]="polyreg"
-# meth[c("lonely_pw")]="polyreg"
-# meth[c("enjoy_life_pw")]="polyreg"
-# meth[c("job_status")]="polyreg"
-# meth[c("help_friends_py")]="polyreg"
-# meth[c("have_friends")]="polyreg"
-# meth[c("difficulty_managing_mny")]="polyreg"
-# meth[c("own_rent_home")]="polyreg"
-# meth[c("social_security")]="polyreg"
-# meth[c("medicare")]="polyreg"
-# meth[c("follow_stockmarket")]="polyreg"
-# meth[c("life_insurance")]="polyreg"
-
-
-#https://cran.r-project.org/web/packages/mice/mice.pdf
-#https://datascienceplus.com/handling-missing-data-with-mice-package-a-simple-approach/
 imputation_one <- mice(dat, m = 5, method = "rf")
 complete_dat <- complete(imputation_one)
 
 
 #check for missingness after imputation
 sapply(complete_dat, function(x) sum(is.na(x)))
+  #saveRDS(complete_dat, file="HRSdat_mice_rf01.rds")
 
+
+imputation_cart <- mice(dat, m = 5, method = "cart")
+complete_dat_cart <- complete(imputation_cart)
+
+
+#check for missingness after imputation
+sapply(complete_dat_cart, function(x) sum(is.na(x)))
+  #saveRDS(complete_dat_cart, file="HRSdat_mice_cart01.rds")
+
+
+
+"
+The data is assumed to be Missing at Random (MAR) because the missingness is explained by groups within the observed data.  
+For example, approximately 89% of the missingness in the *life_satisfaction* variable is from retired individuals.  
+Those with missingness in the *life_satisfaction* were 79 years old and those without missing values were 67 years old.  
+These clear groupings within missing columns is seen for all independent variables.  
+This led to the assumption that data is MAR.   
+Since the data is MAR, Mutiple Imputation by Chained Equations (MICE) is used to impute missing data in the independent variables. 
+The Classification and Regression Tree (CART)/Random Forest method is used to impute values for missing data. 
+CART is selected for its robustness and ability to impute data on continuous and categorical variables. 
+Furthermore, the data shows complex colinearity, interactions, non-linear relationships, and multimodal distributions.  
+CART was preferred over traditional linear and logistic regression MICE techniques to handle these issues. 
+"
