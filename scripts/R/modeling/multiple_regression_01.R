e1f8d17c4c97c06e35a451553aa6852c21f7f1c1
delashu <53063128+delashu@users.noreply.github.com>
2021-11-04 10:38:59 -0400

    regression script edits

---
+++ scripts/R/modeling/multiple_regression_01.R
@@ -0,0 +1,27 @@
+library(MASS)
+
+#multiple linear regression script
+dat <- readRDS(file = "HRSdat_mice_rf01.rds")
+
+#fit multiple linear regression
+fit1 <- lm(net_assets ~ ., data = dat)
+
+#check the assumptions:
+plot(fit1,1, main = c("naive multiple linear regression")) 
+#assumption of linearity between Xis and Y does not hold. 
+
+#try to transform the outcome: 
+
+#fit the model: 
+fit2 <- lm(log(net_assets+1) ~ ., data = dat)
+
+#check the assumptions:
+plot(fit2,1, main = c("naive multiple linear regression"))
+#assumption of linearity between Xis and Y does not hold after transformation 
+
+#since transformations do not hold, we will try another regression method: 
+
+
+#################### Robust Regression ####################
+robust_fit1 <- rlm(net_assets ~ ., data = dat)
+summary(robust_fit1)
